0a1638fdccb9c82ef050a944bfdf5180e2bcda86
Alexandre Santos <alexandre.santozz@gmail.com>
2020-02-13 17:59:10 +0000

    Add check for isDev and show devtools

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
   )
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  isDev && win.webContents.openDevTools()
 
   win.on("closed", () => (mainWindow = null));
 }
