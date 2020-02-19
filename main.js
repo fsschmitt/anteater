cc90d659f5d7fc936e19543ee94296f89fe61d0e
fsschmitt <492108+fsschmitt@users.noreply.github.com>
2020-02-19 09:28:51 +0000

    Add sentry.io to Anteater

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const path = require('path')
 const appSettings = require('./appSettings');
 let win;
 require('./eventBus');
+require('./sentry');
 
 let isDev = !app.isPackaged
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     show: false,
@@ -18,6 +19,7 @@ function createWindow () {
       nodeIntegration: true,
       scrollBounce: true,
       devTools: isDev,
+      preload: path.join(__dirname, 'sentry.js')
     },
     icon: path.join(__dirname, 'assets/icons/icon.png')
   })
@@ -68,3 +70,13 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow)
+
+ipcMain.on('anteater.error', () => {
+  console.log('Error triggered in main processes');
+  throw new Error('Error triggered in main processes');
+});
+
+ipcMain.on('anteater.crash', () => {
+  console.log('process.crash()');
+  process.crash();
+});
---
+++ sentry.js
@@ -0,0 +1,6 @@
+const { init } = require('@sentry/electron');
+
+init({
+  dsn: 'https://2ab223652302404e8d4f99f36d16debf@sentry.io/2638152',
+  debug: true,
+});
