f0b76f1c472c26cd68b2a3deb08608242b2f829c
Alexandre Santos <alexandre.santozz@gmail.com>
2020-02-21 15:52:40 +0000

    fix(logging): send proper user to sentry

--- electron/index.js
+++ electron/index.js
@@ -1,24 +1,23 @@
-
 window.require("../sentry");
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App";
-import * as util from './src/utils/userSettings'
-import * as Sentry from '@sentry/electron'
+import * as util from "./src/utils/userSettings";
+import * as Sentry from "@sentry/electron";
 
-const { ipcRenderer } = window.require('electron');
+const { ipcRenderer } = window.require("electron");
 const { crash } = global.process || {};
 
 window.errorMain = () => {
-  ipcRenderer.send('anteater.error');
+  ipcRenderer.send("anteater.error");
 };
 
 window.errorRenderer = () => {
-  throw new Error('Error triggered in renderer process');
+  throw new Error("Error triggered in renderer process");
 };
 
 window.crashMain = () => {
-  ipcRenderer.send('anteater.crash');
+  ipcRenderer.send("anteater.crash");
 };
 
 window.crashRenderer = crash;
@@ -26,13 +25,13 @@ window.crashRenderer = crash;
 window.versions = {
   chrome: process.versions.chrome,
   electron: process.versions.electron,
-  node: process.versions.node,
+  node: process.versions.node
 };
 
-util.getAll().then((settings) => {
-  Sentry.configureScope((scope) => {
-    scope.setUser(settings.username);
-  })
+util.getAll().then(settings => {
+  Sentry.configureScope(scope => {
+    scope.setUser({ email: settings.username });
+  });
 });
 
 ReactDOM.render(<App />, document.getElementById("root"));
